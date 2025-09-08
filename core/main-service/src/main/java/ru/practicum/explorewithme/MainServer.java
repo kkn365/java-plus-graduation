55624de0a1e08f2aa3249577fa76f8f1391e6c20
kkn365 <KvitchenkoKN@yandex.ru>
2025-09-08 15:44:58 +0300

    Fix build errors.

--- core/main-service/src/main/java/ru/practicum/explorewithme/MainServer.java
+++ core/main-service/src/main/java/ru/practicum/explorewithme/MainServer.java
@@ -2,10 +2,12 @@ package ru.practicum.explorewithme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class MainServer {
     public static void main(String[] args) {
         SpringApplication.run(MainServer.class, args);
