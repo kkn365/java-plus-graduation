8804a6aec18f1ee24f6973ab0884f2aa8c74d058
kkn365 <KvitchenkoKN@yandex.ru>
2025-09-08 18:07:15 +0300

    Fix postman test fail.

--- core/main-service/src/main/java/ru/practicum/explorewithme/events/service/EventServiceImpl.java
+++ core/main-service/src/main/java/ru/practicum/explorewithme/events/service/EventServiceImpl.java
@@ -168,7 +168,12 @@ public class EventServiceImpl implements EventService {
                 List.of("/events/" + event.getId()),
                 false);
 
-        event.setViews(hitsStatDTOS.get(0).getHits());
+        if (!hitsStatDTOS.isPresent()) {
+            return event;
+        }
+        HitsStatDTO hitsStatDTO = hitsStatDTOS.get().stream().findFirst()
+                .orElseThrow(() -> new NotFoundException("Event with id=" + eventId + " was not found"));
+        event.setViews(hitsStatDTO.getHits());
         eventRepository.save(event);
         return event;
     }
