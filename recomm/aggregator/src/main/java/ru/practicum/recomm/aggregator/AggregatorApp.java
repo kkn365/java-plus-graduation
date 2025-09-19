1043a215a4e5ef721c83a43a3a36dd62331b0189
kkn365 <KvitchenkoKN@yandex.ru>
2025-09-19 14:04:56 +0300

    Fix Eureka registrations for aggregator.

--- recomm/aggregator/src/main/java/ru/practicum/recomm/aggregator/AggregatorApp.java
+++ recomm/aggregator/src/main/java/ru/practicum/recomm/aggregator/AggregatorApp.java
@@ -2,17 +2,10 @@ package ru.practicum.recomm.aggregator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(exclude = {
-        DataSourceAutoConfiguration.class,
-        JpaRepositoriesAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 @EnableDiscoveryClient
 @ConfigurationPropertiesScan
 public class AggregatorApp {
